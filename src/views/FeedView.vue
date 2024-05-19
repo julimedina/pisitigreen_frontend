af63712238bb2589bfc00da55e1111ad164ebb41
juliana <juliana.medina.peaku.io>
2024-05-19 14:28:01 -0500

    Renormalize line endings

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -24,12 +24,12 @@
 
 
     </div> 
-    <div class="col-sm-5 ms-5 mt-5 ">
+    <div class="col-sm-5 ms-0 me-0 mt-5 ">
 
         <div>
-        <div class=" row-sm d-flex flex-column mb-3 container-publicacion  ">
-            <div class="p-2 mt-5 d-flex justify-center">
-                <textarea class="form-control bg-light ms-4"  placeholder="Qué estás pensando?" aria-label="With textarea" ></textarea>
+            <div class=" row-sm d-flex flex-column mb-3 container-publicacion  ">
+                <div class="p-2 mt-5 d-flex justify-center">
+                    <textarea class="form-control bg-light ms-4"  placeholder="Qué estás pensando?" aria-label="With textarea" ></textarea>
                 </div>
             
                 <div class="row p-2">
@@ -41,8 +41,8 @@
     </div>
    
         <div class="d-flex flex-column mb-3 container-publicacion ">
-            <div class="row">
-                <div class=" col col-lg-2">
+            <div class="row ">
+                <div class=" col col-lg-2  ">
                     <img src="../assets/feed.jpg" class=" ms-3 mt-3 img-thumbnail img-min-perfil" alt="...">
 
                 </div>
@@ -52,26 +52,136 @@
                 <div class="col">
                     <p class="mt-5 ms-5">Hace 18 minutos</p>
                 </div>
-
+                
             </div> 
+            <div class="row">
+                <div class="col p-4">
+                    <img src="../assets/img1.png" class="img-fluid rounded" alt="...">
+                </div>
+
+                
+            </div>
+
+            <div class="row d-flex justify-start ms-3 mb-4">
+                <div class="col-3">
+                    <a href="#">
+                        <span class="material-icons-outlined">
+                        thumb_up
+                    </span>
+                    <p>
+                        82 Likes
+
+                    </p>
+                    </a>
+                    
+                    
+                </div>
+
+                <div class="col-3 ">
+                   <a href="#">
+                    <span class="material-icons-outlined">
+                        sms
+                    </span>
+                    <p>
+                        0 comentarios
+
+                    </p>
+                   </a> 
+                    
+                </div>
+
+
+
+            </div>
+            
+               
+
+
+           
         </div>
    
 
-</div>
-    
-
-    
-
-
-
-
-
-
-    <div class=" col">
-        
-        
     </div>
+    
 
+    
+
+
+
+
+
+    <div class="col  mt-5 ms-0">
+
+<div>
+    <div class=" row-sm d-flex flex-column mb-3 container-publicacion  ">
+        <div class="text-center mt-5">
+            <strong>
+                <h1>
+                Gente que quizás conoces
+            </h1>
+            </strong>
+            
+
+        </div>
+    
+        <div class="row">
+            <div class="col">
+                <img src="../assets/feed.jpg" class=" ms-3 mt-3 img-thumbnail img-min-perfil" alt="...">
+
+            </div>
+            <div class="col mt-5">
+                <strong><h1>Juliana Medina</h1></strong>
+
+            </div>
+
+            <div class="col mt-5 mb-5 ms-5 me-5">
+                <button type="button" class="btn btn-outline-primary">Mostrar</button>
+
+            </div>
+
+        </div> 
+</div>
+</div>
+
+<div>
+    <div class=" row-sm d-flex flex-column mb-3 container-publicacion  ">
+        <div class="text-center mt-5">
+            <strong>
+                <h1>
+                Trends
+            </h1>
+            </strong>
+            
+
+        </div>
+    
+        <div class="row">
+           
+            <div class="col mt-5 ms-4 mb-0">
+                <strong><p>#Juliana Medina</p></strong>
+                
+                    <p >
+                        180 Posts
+                    </p>
+                 
+
+            </div>
+           
+
+            <div class="col mt-5 mb-5 ms-5 me-5">
+                <button type="button" class="btn btn-outline-primary">Explorar</button>
+
+            </div>
+
+         
+
+        </div> 
+</div>
+</div>
+
+
+</div>    
+        
 </div>
 
 </template>
@@ -91,7 +201,7 @@
 }
 
 .container-publicacion{
-    height: 50%;
+    
     border: 1px lightgrey solid;
     border-radius: 5px;
 }
