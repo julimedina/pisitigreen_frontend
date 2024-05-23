3932d60447e24667158814ebaf38ff0890298df2
juliana <juliana.medina.peaku.io>
2024-05-23 17:05:24 -0500

    Se agregan cambios al archivo app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,112 @@
 <template>
-    <NavPrincipal/>
 
- 
+<nav class="bg-gray-800">
+  <div v-if="userStore.user.isAuthenticated" class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="relative flex h-16 items-center justify-between">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="absolute -inset-0.5"></span>
+          <span class="sr-only">Open main menu</span>
+          <!--
+            Icon when menu is closed.
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-shrink-0 items-center">
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+        </div>
+      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
+        
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex space-x-4">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons"> other_houses</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">forum</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">notifications_active</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">search</span></a>
+           
+           
+          </div>
+        </div>
+      </div>
+      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        
+
+        <!-- Profile dropdown -->
+        <div class="relative ml-3">
+          <div>
+            <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+              <span class="absolute -inset-1.5"></span>
+              <span class="sr-only">Open user menu</span>
+              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            </button>
+          </div>
+
+         
+       
+        </div>
+
+
+      </div>
+    </div>
+    <div  class="menu-right">
+      <template  v-if="userStore.user.isAuthenticated">
+       
+        <a href="">
+        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+
+      </a>
+      </template>
+      <template v-else="">
+        <div>
+          <router-link to="/Login" class="mr-4 rounded-md px-3 py-2 text-sm font-medium bg-indigo-400">Iniciar Sesión</router-link>
+          <router-link to="/SignUp" class="mr-4 rounded-md px-3 py-2 text-sm font-medium bg-indigo-400">Registrarse</router-link>
+        </div>
+      </template>
+     
+
+    </div>
+
+  </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="space-y-1 px-2 pb-3 pt-2 flex justify-center">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons"> other_houses</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">forum</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">notifications_active</span></a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><span class="material-icons">search</span></a>
+    </div>
+  </div>
+
+</nav>
+<main>
   <router-view/>
- 
+</main>
+
   <Toast/>
 </template>
 
 
 <script>
 import axios from 'axios';
-import NavPrincipal from './components/NavPrincipal.vue';
+
 import Toast from './components/Toast.vue'
 import { useUserStore } from './store/user';
 
@@ -27,13 +123,13 @@ export default{
   name:'App',
 
   components:{
-    NavPrincipal,
+   
     Toast
     
   },
   beforeCreate(){
     this.userStore.initStore()
-    const token= this.useUserStore.user.access
+    const token= this.userStore.user.access
     if (token){
     axios.defaults.headers.common["Authorization"] = "Bearer " + token;
   }else{
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -9,18 +9,79 @@
 
         <div>
             <div class=" row-sm d-flex flex-column mb-3 container-publicacion bg-light ">
+                <form action="" method="post" v-on:submit.prevent="submitForm">
                 <div class="p-2 mt-5 d-flex justify-center">
-                    <textarea class="form-control  ms-4 me-4"  placeholder="Qué estás pensando?" aria-label="With textarea" ></textarea>
+                    <textarea v-model="body" class="form-control  ms-4 me-4"  placeholder="Qué estás pensando?" aria-label="With textarea" ></textarea>
                 </div>
             
                 <div class="row p-2">
                    <div class="col-sm ms-5 mb-3"><button type="button" class="btn btn-outline-primary ">Adjuntar imagen</button></div> 
                    <div class="col-sm  ms-5 mb-3"><button type="button" class="btn btn-outline-secondary ">Posts</button></div> 
                   
-                </div>  
+                </div> 
+            </form> 
         </div>
     </div>
-   
+
+
+    <div class="d-flex flex-column mb-3 container-publicacion bg-light">
+            <div class="row ">
+                <div class=" col col-lg-2  ">
+                    <img src="../assets/feed.jpg" class=" ms-3 mt-3 img-thumbnail img-min-perfil" alt="...">
+
+                </div>
+                <div class="col">
+                    <strong><h1 class="md-auto mt-5">{{ post.created_by.name }}</h1></strong>
+                </div>
+                <div class="col">
+                    <p class="mt-5 ms-5">hace {{ post.created_at_formatted }}</p>
+                </div>
+                
+            </div> 
+            <div class="row">
+               
+                <div class="col p-4" v-for="post in posts" v-bind:key >
+                    {{ post.body }}
+                </div>
+                
+            </div>
+
+            
+
+            <div class="row d-flex justify-start ms-3 mb-4">
+                <div class="col-3">
+                    <a href="#">
+                        <span class="material-icons-outlined">
+                        thumb_up
+                    </span>
+                    <p>
+                        82 Likes
+
+                    </p>
+                    </a>
+                    
+                    
+                </div>
+
+                <div class="col-3 ">
+                   <a href="#">
+                    <span class="material-icons-outlined">
+                        sms
+                    </span>
+                    <p>
+                        0 comentarios
+
+                    </p>
+                   </a> 
+                    
+                </div>
+
+
+
+            </div>
+
+           
+        </div>
         <div class="d-flex flex-column mb-3 container-publicacion bg-light">
             <div class="row ">
                 <div class=" col col-lg-2  ">
@@ -36,12 +97,14 @@
                 
             </div> 
             <div class="row">
+               
                 <div class="col p-4">
                     <img src="../assets/img1.png" class="img-fluid rounded" alt="...">
                 </div>
-
                 
             </div>
+
+            
 
             <div class="row d-flex justify-start ms-3 mb-4">
                 <div class="col-3">
@@ -94,6 +157,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import PersonasQueQuizasConozcas from '../components/PersonasQueQuizaConozcas.vue';
 import TrendsContainer from '../components/TrendsContainer.vue';
 import PerfilContainer from '@/components/PerfilContainer.vue';
@@ -105,6 +169,42 @@ export default{
     TrendsContainer,
     PerfilContainer
     
+  },
+
+  data() {
+    return{
+        posts:[],
+        body:'',
+    }
+  },
+  mounted(){
+    this.getFeed()
+  },
+  methods:{
+    getFeed(){
+        axios
+            .get('/api/posts/')
+            .then(response =>{
+                console.log('data', response.data)
+                this.posts = response.data
+            })
+            .catch(error=>{
+                console.log('error', error)
+            })
+    },
+    submitForm(){
+        console.log('submitForm', this.body)
+        axios
+        .post('/api/posts/create/',{
+            'body': this.body
+        })
+        .then (response=>{
+            console.log('data',response.data)
+        })
+        .catch(error=>{
+            console.log('error',error)
+        })
+    }
   }
 }
 
