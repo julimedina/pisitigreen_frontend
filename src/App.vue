0935d46768ecf752dbc038864450be6389dd7660
juliana <juliana.medina.peaku.io>
2024-05-23 08:01:08 -0500

    Se agregan cambios al archivo user.js

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,36 @@
 
 
 <script>
+import axios from 'axios';
 import NavPrincipal from './components/NavPrincipal.vue';
 import Toast from './components/Toast.vue'
+import { useUserStore } from './store/user';
 
 
 
 export default{
+
+  setup(){
+    const userStore =useUserStore()
+    return{
+      userStore
+    }
+  },
   name:'App',
 
   components:{
     NavPrincipal,
     Toast
     
+  },
+  beforeCreate(){
+    this.userStore.initStore()
+    const token= this.useUserStore.user.access
+    if (token){
+    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+  }else{
+    axios.defaults.headers.common["Authorization"] = "";
   }
+ }
 }
 </script>
